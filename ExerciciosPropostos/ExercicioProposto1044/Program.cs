94fb5bc1a0c1f09a8c512c4afbd367e2a5e49424
rafaelbriet <rafael-briet@hotmail.com>
2019-04-20 19:51:07 -0300

    Excercício 1044 URI

---
+++ ExerciciosPropostos/ExercicioProposto1044/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProposto1044
+{
+
+	// DESCRIÇÃO: Lê dois números inteiros e determina se são ou não múltiplos
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string[] entrada;
+			int a, b;
+
+			entrada = Console.ReadLine().Split();
+
+			a = int.Parse(entrada[0]);
+			b = int.Parse(entrada[1]);
+
+			if (a % b == 0 || b % a == 0)
+			{
+				Console.WriteLine("Sao Multiplos");
+			}
+			else
+			{
+				Console.WriteLine("Nao sao Multiplos");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
