5fd2a6324574ce10fbc590c7b2735e8af92e5437
rafaelbriet <rafael-briet@hotmail.com>
2019-04-28 14:41:49 -0300

    Exercício 1049 URI.

---
+++ ExerciciosPropostos/ExercicioProposto1049/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioProposto1049
+{
+
+	// DESCRIÇÃO: Lê 3 palavras que definem o tipo de animal.
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a, b, c, animal;
+
+			a = Console.ReadLine();
+			b = Console.ReadLine();
+			c = Console.ReadLine();
+
+			if (a == "vertebrado")
+			{
+				if (b == "ave")
+				{
+					if (c == "carnivoro")
+					{
+						animal = "aguia";
+					} else
+					{
+						animal = "pomba";
+					}
+				} else
+				{
+					if (c == "onivoro")
+					{
+						animal = "homem";
+					}
+					else
+					{
+						animal = "vaca";
+					}
+				}
+			}
+			else {
+				if (b == "inseto")
+				{
+					if (c == "hematofago")
+					{
+						animal = "pulga";
+					}
+					else
+					{
+						animal = "lagarta";
+					}
+				}
+				else
+				{
+					if (c == "hematofago")
+					{
+						animal = "sanguessuga";
+					}
+					else
+					{
+						animal = "minhoca";
+					}
+				}
+			}
+
+			Console.WriteLine(animal);
+
+			Console.ReadLine();
+		}
+	}
+}
