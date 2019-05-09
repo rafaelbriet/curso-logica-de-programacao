588ae8608db9e991cd394821f2e7d76af3cd0b49
Rafael Briet <rafael-briet@hotmail.com>
2019-05-09 00:10:23 -0300

    Exercício resolvido apresentado em vídeo pelo professor durante o curso.

---
+++ ExerciciosResolvidos/ExercicioResolvido09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioResolvido09
+{
+
+	//DESCRIÇÃO: Recebe uma série de números e calcula a média entre eles.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double idadeSomada = 0;
+			int vezes = 0;
+			double media;
+
+			int idade = int.Parse(Console.ReadLine());
+
+			if (idade < 0)
+			{
+				Console.WriteLine("impossivel calcular");
+			}
+			else
+			{
+				while (idade >= 0)
+				{
+					idadeSomada += idade;
+
+					vezes++;
+
+					idade = int.Parse(Console.ReadLine());
+				}
+
+				media = idadeSomada / vezes;
+
+				Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
