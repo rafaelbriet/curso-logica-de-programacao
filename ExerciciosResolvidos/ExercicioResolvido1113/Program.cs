1ef0a6277cb1290421ece447d331e2592e3c000e
Rafael Briet <rafael-briet@hotmail.com>
2019-05-08 23:42:11 -0300

    Exercício Resolvido URI 1113.

---
+++ ExerciciosResolvidos/ExercicioResolvido1113/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioResolvido1113
+{
+
+	// DESCRIÇÃO: Recebe uma dubla de valores e determina se eles estão em ordem crescente ou decrescente.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] entrada;
+			int x, y;
+
+			entrada = Console.ReadLine().Split(' ');
+
+			x = int.Parse(entrada[0]);
+			y = int.Parse(entrada[1]);
+
+			while(x != y)
+			{
+				if (x > y)
+				{
+					Console.WriteLine("Decrescente");
+				}
+				else
+				{
+					Console.WriteLine("Crescente");
+				}
+
+				entrada = Console.ReadLine().Split(' ');
+
+				x = int.Parse(entrada[0]);
+				y = int.Parse(entrada[1]);
+			}
+
+
+			Console.ReadLine();
+		}
+	}
+}
