b6748775a85de10e7aafb470dba96e45f3cbc6ea
Rafael Briet <rafael-briet@hotmail.com>
2019-05-27 14:10:23 -0300

    Exercício 1117 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1117/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1117
+{
+	// DESCRIÇÃO: Recebe duas notas de um aluno e calcula a média.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double notaUm, notaDois, media;
+
+			notaUm = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			while (notaUm < 0 || notaUm > 10)
+			{
+				Console.WriteLine("nota invalida");
+				notaUm = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			}
+
+			notaDois = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			while (notaDois < 0 || notaDois > 10)
+			{
+				Console.WriteLine("nota invalida");
+				notaDois = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			}
+
+			media = (notaUm + notaDois) / 2;
+
+			Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+			Console.ReadLine();
+		}
+	}
+}
