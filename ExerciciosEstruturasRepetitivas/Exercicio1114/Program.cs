40ba9b2bb685c34ac504e8f7d1244cd43bee6ec0
Rafael Briet <rafael-briet@hotmail.com>
2019-05-26 22:54:50 -0300

    Exercício 1114 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio1114
+{
+
+	// DESCRIÇÃO: Recebe um senha e verifica se está correta.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int senha = 2002;
+
+			int entrada = int.Parse(Console.ReadLine());
+
+			while (entrada != senha)
+			{
+				Console.WriteLine("Senha Invalida");
+
+				entrada = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine("Acesso Permitido");
+
+			Console.ReadLine();
+		}
+	}
+}
