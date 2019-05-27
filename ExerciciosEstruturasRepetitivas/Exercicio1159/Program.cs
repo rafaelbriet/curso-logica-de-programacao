61f99e923322fe7eb8c07f35fc16b17c127b94c7
Rafael Briet <rafael-briet@hotmail.com>
2019-05-27 15:15:33 -0300

    Exercício 1159 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1159/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1159
+{
+
+	// DESCRIÇÃO: Recebe um valor e calcula a soma dos dos próximos cinco números pares (incluindo ele caso seja par).
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int soma;
+			int x = int.Parse(Console.ReadLine());
+
+			while (x != 0)
+			{
+				if (x % 2 != 0)
+				{
+					x += 1;
+				}
+
+				soma = x + (x + 2) + (x + 4) + (x + 6) + (x + 8);
+
+				Console.WriteLine(soma.ToString());
+				x = int.Parse(Console.ReadLine());
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
