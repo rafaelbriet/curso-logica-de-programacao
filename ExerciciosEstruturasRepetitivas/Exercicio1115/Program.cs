c6a466756d7c01f0b67527a496d42d23a2afd0f3
Rafael Briet <rafael-briet@hotmail.com>
2019-05-26 23:18:36 -0300

    Exercício 1115 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1115/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1115
+{
+
+	// DESCRIÇÃO: Recebe dois pontos do sistema cartesiano e determina à qual quadrante pertence as coordenadas.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] coordenadas;
+			string mensagem;
+			int x, y;
+
+			coordenadas = Console.ReadLine().Split(' ');
+
+			x = int.Parse(coordenadas[0]);
+			y = int.Parse(coordenadas[1]);
+
+			while (x != 0 && y != 0)
+			{
+
+				if (x > 0 && y > 0)
+				{
+					mensagem = "primeiro";
+				}
+				else if (x < 0 && y > 0)
+				{
+					mensagem = "segundo";
+				}
+				else if (x < 0 && y < 0)
+				{
+					mensagem = "terceiro";
+				}
+				else
+				{
+					mensagem = "quarto";
+				}
+
+				Console.WriteLine(mensagem);
+
+				coordenadas = Console.ReadLine().Split(' ');
+
+				x = int.Parse(coordenadas[0]);
+				y = int.Parse(coordenadas[1]);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
