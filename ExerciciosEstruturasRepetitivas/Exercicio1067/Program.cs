8d90a9edf7eab9a46c85af2dd2420fd2f70e4186
Rafael Briet <rafael-briet@hotmail.com>
2019-05-30 17:53:21 -0300

    Exercício 1067 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1067/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1067
+{
+	// DESCRIÇÃO: Lê um número e exibe todos os números até ele.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int x = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= x; i++)
+			{
+				if (i % 2 != 0)
+				{
+					Console.WriteLine(i);
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
