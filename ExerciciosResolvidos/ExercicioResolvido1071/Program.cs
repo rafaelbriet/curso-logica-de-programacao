f6496fd28fd657dcd6da065e7594c0c31e454799
Rafael Briet <rafael-briet@hotmail.com>
2019-05-30 16:10:24 -0300

    Exercícios 1071 e 1078 do URI resolvidos acompanhando o video do curso.

---
+++ ExerciciosResolvidos/ExercicioResolvido1071/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvido1071
+{
+	// DESCRIÇÃO: Lê 2 valores inteiros e calcula a soma dos números impares entre eles.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int x, y, min, max, soma = 0;
+
+			x = int.Parse(Console.ReadLine());
+			y = int.Parse(Console.ReadLine());
+
+			if (x < y)
+			{
+				min = x;
+				max = y;
+			} else
+			{
+				min = y;
+				max = x;
+			}
+
+			for (int i = min + 1; i < max; i++)
+			{
+				if (i % 2 != 0)
+				{
+					soma += i;
+				}
+			}
+
+			Console.WriteLine(soma);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ExerciciosResolvidos/ExercicioResolvido1078/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvido1078
+{
+
+	// DESCRIÇÃO: Lê um número e mostra a tabuada dele.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= 10; i++)
+			{
+				int resultado = i * n;
+				Console.WriteLine(i + " x " + n + " = " + resultado);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
