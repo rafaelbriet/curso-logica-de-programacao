20b2005bd8755b55339ed6e2651b5874b8f288a9
Rafael Briet <rafael-briet@hotmail.com>
2019-05-31 13:56:15 -0300

    Excercício 1073 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1073/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1073
+{
+	class Program
+	{
+
+		// DESCRIÇÃO: Lê um valor e exebibe o quadrado dos números pares até ele.
+
+		static void Main(string[] args)
+		{
+
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (i % 2 == 0)
+				{
+					int quadrado = (int)Math.Pow(i, 2.0);
+					Console.WriteLine(i + "^2 = " + quadrado);
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
