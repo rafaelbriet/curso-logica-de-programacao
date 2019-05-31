ab226ab9785aecb612e0cf312c9993a22e1002c7
Rafael Briet <rafael-briet@hotmail.com>
2019-05-31 14:19:57 -0300

    Excercício 1075 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1075/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1075
+{
+
+	// DESCRIÇÃO: Lê um número é exibe todos os números entre 1 e 10000 que dividido por ele tenha resto 2.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i < 10000; i++)
+			{
+				if (i % n == 2)
+				{
+					Console.WriteLine(i);
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
