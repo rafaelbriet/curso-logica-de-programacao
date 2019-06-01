4c975b89308e1f984843586944d3705cddc2cebe
rafaelbriet <rafael-briet@hotmail.com>
2019-06-01 17:29:03 -0300

    Excercício 1096 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1096/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1096
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (i % 2 != 0)
+				{
+					for (int j = 7; j > 4; j--)
+					{
+						Console.WriteLine("I=" + i + " " + "J=" + j);
+					}					
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
