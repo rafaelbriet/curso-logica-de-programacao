6bb5733db50f138e5f21c3ca69c269d24dbd6c7e
rafaelbriet <rafael-briet@hotmail.com>
2019-06-01 19:25:29 -0300

    Exercício 1097 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1097/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1097
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 7;
+
+			for (int i = 0; i < 10; i++)
+			{
+				if (i % 2 != 0)
+				{
+					for (int j = x; j > x - 3; j--)
+					{
+						Console.WriteLine("I=" + i + " J=" + j);
+					}
+
+					x += 2;
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
