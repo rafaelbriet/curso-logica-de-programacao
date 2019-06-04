648320c61d00baed622131a9e1d4f5f50cfc605d
Rafael Briet <rafael-briet@hotmail.com>
2019-06-04 11:41:17 -0300

    Exercício 1143 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1143/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1143
+{
+	// DESCRIÇÃO: Lê um um número que representa a quantidade de saída.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, quadrado, cubo;
+
+			n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= n; i++)
+			{
+				quadrado = (int)Math.Pow((double)i, 2.0);
+				cubo = (int)Math.Pow((double)i, 3.0);
+
+				Console.WriteLine(i + " " + quadrado + " " + cubo);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
