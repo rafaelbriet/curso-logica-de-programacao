7563433ea70a961761b271968f4546747a0ca959
Rafael Briet <rafael-briet@hotmail.com>
2019-06-04 11:25:16 -0300

    Exercício 1142 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1142/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1142
+{
+	// DECRIÇÃO: Lê um número que representa a quantidade de saída que será exibida.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, x = 1;
+
+			n = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = x; j <= x + 2; j++)
+				{
+					Console.Write(j + " ");
+				}
+
+				x += 4;
+
+				Console.WriteLine("PUM");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
