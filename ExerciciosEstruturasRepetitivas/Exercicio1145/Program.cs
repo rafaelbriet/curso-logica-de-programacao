b5f7b92d6372cdd3421d0ac9c3e3e4ce33ca1c96
Rafael Briet <rafael-briet@hotmail.com>
2019-06-09 20:50:43 -0300

    Exercício 1145 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1145/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1145
+{
+	class Program
+	{
+		// DESCRIÇÃO: Lê dois valores X e Y. A seguir, mostra uma 
+		// sequência de 1 até Y, passando para a próxima linha a cada X números.
+
+		static void Main(string[] args)
+		{
+			string[] entrada;
+			int x, y;
+
+			entrada = Console.ReadLine().Split();
+
+			x = int.Parse(entrada[0]);
+			y = int.Parse(entrada[1]);
+
+			for (int i = 1; i <= y; i++)
+			{
+				if (i % x == 0)
+				{
+					Console.WriteLine(i);
+				}
+				else
+				{
+					Console.Write(i + " ");
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
