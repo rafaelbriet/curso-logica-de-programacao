12b94b9f936e344ab6532225b6959b490a1cce9f
Rafael Briet <rafael-briet@hotmail.com>
2019-06-08 22:11:32 -0300

    Exercício 1153 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1153/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1153
+{
+	// DESCRIÇÃO: Calcula o fatorial de um número
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, fatorial;
+
+			n = int.Parse(Console.ReadLine());
+
+			fatorial = n;
+
+			for (int i = n - 1; i > 1; i--)
+			{
+				fatorial *= i;
+			}
+
+			Console.WriteLine(fatorial.ToString());
+			Console.ReadLine();
+		}
+	}
+}
