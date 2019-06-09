dd8c0472559f46d2d73703c3047e489d6a3d9320
Rafael Briet <rafael-briet@hotmail.com>
2019-06-08 22:39:45 -0300

    Exercício 1156 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1156/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1155
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double s = 1.0;
+			double dividendo = 3.0;
+			double divisor = 2.0;
+
+			for (int i = 1; i <= 18; i++)
+			{
+				s += dividendo / divisor;
+				dividendo += 2.0;
+				divisor *= 2.0;
+
+				//Console.WriteLine(dividendo.ToString("F2", CultureInfo.InvariantCulture) + " : " + divisor.ToString("F2", CultureInfo.InvariantCulture));
+			}
+
+			Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+			Console.ReadLine();
+		}
+	}
+}
+
