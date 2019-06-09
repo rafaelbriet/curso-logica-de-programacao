0e7b01aedc934bcc4ed062646f31ed7270470a7a
Rafael Briet <rafael-briet@hotmail.com>
2019-06-08 22:19:32 -0300

    Exercício 1155 URI.

---
+++ ExerciciosEstruturasRepetitivas/Exercicio1155/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1155
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double s = 1.0;
+
+			for (int i = 2; i <= 100; i++)
+			{
+				s += 1.0 / i;
+			}
+
+			Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+			Console.ReadLine();
+		}
+	}
+}
