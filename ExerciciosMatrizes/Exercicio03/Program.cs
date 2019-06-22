65492a759d91a7cab117158b839f30edd4c2e85e
rafaelbriet <rafael-briet@hotmail.com>
2019-06-22 16:08:09 -0300

    Exercício 03 de matriz.

---
+++ ExerciciosMatrizes/Exercicio03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio03
+{
+	class Program
+	{
+		// DESCRIÇÃO: Lê uma matriz quadrada e exibe o maior elemento de cada linha
+
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int[,] matriz = new int[n, n];
+
+			string[] entrada;
+			for (int i = 0; i < n; i++)
+			{
+				entrada = Console.ReadLine().Split(' ');
+				for (int j = 0; j < n; j++)
+				{
+					matriz[i, j] = int.Parse(entrada[j]);
+				}
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				int maior = 0;
+				for (int j = 0; j < n; j++)
+				{
+					if (matriz[i, j] > maior)
+					{
+						maior = matriz[i, j];
+					}
+				}
+
+				Console.WriteLine($"{maior}");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
